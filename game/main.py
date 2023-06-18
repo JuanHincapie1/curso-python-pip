e8e9f468fb9e8563a6d3b3882307ccc77682680c
Juan Hincapie <juahincapiemo@unal.edu.com>
2023-06-18 16:22:14 -0500

    Juego profesor

--- game/main.py
+++ game/main.py
@@ -1,39 +1,23 @@
 import random
 
-options = ('piedra', 'papel', 'tijera')
-
-computer_wins = 0
-user_wins = 0
-
-rounds = 1
 
 def choose_options():
-    
+  options = ('piedra', 'papel', 'tijera')
   user_option = input('piedra, papel o tijera => ')
   user_option = user_option.lower()
 
-while True:
-
-  print('*' * 10)
-  print('ROUND', rounds)
-  print('*' * 10)
-
-  print('computer_wins', computer_wins)
-  print('user_wins', user_wins)
-
-
-
-  rounds += 1
-
   if not user_option in options:
     print('esa opcion no es valida')
-    continue
+    # continue
+    return None, None
 
   computer_option = random.choice(options)
 
   print('User option =>', user_option)
   print('Computer option =>', computer_option)
+  return user_option, computer_option
 
+def check_rules(user_option, computer_option, user_wins, computer_wins):
   if user_option == computer_option:
     print('Empate!')
   elif user_option == 'piedra':
@@ -63,11 +47,30 @@ while True:
       print('piedra gana a tijera')
       print('computer gano!')
       computer_wins += 1
+  return user_wins, computer_wins
 
-  if computer_wins == 2:
-    print('El ganador es la computadora')
-    break
+def run_game():
+  computer_wins = 0
+  user_wins = 0  
+  rounds = 1
+  while True:
+    print('*' * 10)
+    print('ROUND', rounds)
+    print('*' * 10)
 
-  if user_wins == 2:
-    print('El ganador es el usuario')
-    break
+    print('computer_wins', computer_wins)
+    print('user_wins', user_wins)
+    rounds += 1
+
+    user_option, computer_option = choose_options()
+    user_wins, computer_wins = check_rules(user_option, computer_option, user_wins, computer_wins)
+
+    if computer_wins == 2:
+      print('El ganador es la computadora')
+      break
+
+    if user_wins == 2:
+      print('El ganador es el usuario')
+      break
+
+run_game()
